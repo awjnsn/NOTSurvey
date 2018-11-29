460bf9c6eecd133eeca776f1e8a4ce69effcdedb
William Cabell <william16180@gmail.com>
2018-11-28 22:33:03 -0500

    Fixed code style
    That you can does not mean that you should

--- block.js
+++ block.js
@@ -1,10 +1,10 @@
-function try_removal(){
-    try{
-        document.getElementById('ek-modal').remove()
-        document.getElementById('ek-overlay').remove()
-        clearInterval(interval)
+function try_removal() {
+    try {
+        document.getElementById('ek-modal').remove();
+        document.getElementById('ek-overlay').remove();
+        clearInterval(interval);
     }
-    catch(e){}
+    catch(e) {}
 }
 
 let interval = setInterval(try_removal, 100);

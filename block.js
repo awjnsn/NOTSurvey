988887dc9b255be5d7909d78f26d5f767112b21a
Alex Johnson <awjnsn@gmail.com>
2018-11-28 22:45:39 -0500

    Formatting stuff
    Since the included changes are solely aesthetic, an update to the plugin will not be published.

--- block.js
+++ block.js
@@ -1,10 +1,20 @@
-function try_removal(){
-    try{
-        document.getElementById('ek-modal').remove()
-        document.getElementById('ek-overlay').remove()
-        clearInterval(interval)
+function try_removal()
+{
+    try
+    {
+        //Trys to remove the elements, throws exception if the elements have not yet loaded
+        document.getElementById('ek-modal').remove();
+        document.getElementById('ek-overlay').remove();
+        
+        //Stops trying to remove the elements, as the have already been removed
+        clearInterval(interval);
     }
-    catch(e){}
+    
+    catch(e)
+    {
+        //Do nothing, we'll just try again later
+    }
 }
 
+//Attemt to remove the popup until it no longer exists
 let interval = setInterval(try_removal, 100);
